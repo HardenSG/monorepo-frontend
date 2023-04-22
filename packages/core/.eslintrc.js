c8f723dcd1a9353837c97dc7b35e47f9a8405577
SG <2767525216@qq.com>
2023-04-23 00:32:07 +0800

    feat: 新增路径别名

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: ["standard", "plugin:prettier/recommended", "prettier"],
+  parser: "vue-eslint-parser",
+
+  parserOptions: {
+    requireConfigFile: false,
+    sourceType: "module",
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+
+  plugins: ["prettier"],
+
+  rules: {
+    "comma-dangle": "off",
+    "vue/comment-directive": "off",
+    "space-before-function-paren": "off",
+    "vue/multi-word-component-names": "off",
+    "no-undef": "off",
+  },
+
+  overrides: [],
+}
