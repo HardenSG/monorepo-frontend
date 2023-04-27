dbd4b1f73e7d4ff3b137b92a10cff1e7fa3ad0ca
SG <2767525216@qq.com>
2023-04-27 14:56:44 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 /* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'eslint:recommended',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {},
-  globals: {
-    __dirname: 'readonly',
-  },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
